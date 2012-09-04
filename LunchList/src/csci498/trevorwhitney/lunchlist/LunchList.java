79939926d6ed38ec7ed83c8029a7fa6d5c6efedf
Trevor Whitney <trevorjwhitney@gmail.com>
2012-09-04 16:23:14 -0600

    added save button action to LunchList class

--- LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
+++ LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
@@ -2,19 +2,31 @@ package csci498.trevorwhitney.lunchlist;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LunchList extends Activity {
+	
+	Restaurant restaurant = new Restaurant();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
+        
+        Button save = (Button)findViewById(R.id.save_btn);
+        save.setOnClickListener(onSave);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
-        return true;
-    }
+    private View.OnClickListener onSave = new View.OnClickListener() {	
+		public void onClick(View v) {
+			EditText name = (EditText)findViewById(R.id.name);
+			EditText address = (EditText)findViewById(R.id.address);
+			
+			restaurant.setName(name.getText().toString());
+			restaurant.setAddress(address.getText().toString());
+		}
+	};
+
 }
