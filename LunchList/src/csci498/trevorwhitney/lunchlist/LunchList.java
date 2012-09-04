e4c0871d2c8fee845fa5f9dda8727c8cb34c2239
Trevor Whitney <trevorjwhitney@gmail.com>
2012-09-04 17:28:19 -0600

    updated save onClick to save restaurant type to model

--- LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
+++ LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class LunchList extends Activity {
 	
@@ -20,12 +21,28 @@ public class LunchList extends Activity {
     }
 
     private View.OnClickListener onSave = new View.OnClickListener() {	
-		public void onClick(View v) {
+    	public void onClick(View v) {
 			EditText name = (EditText)findViewById(R.id.name);
 			EditText address = (EditText)findViewById(R.id.address);
 			
 			restaurant.setName(name.getText().toString());
 			restaurant.setAddress(address.getText().toString());
+			
+			RadioGroup types = (RadioGroup)findViewById(R.id.types);
+			
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.type_in:
+				restaurant.setType("dine_in");
+				break;
+			
+			case R.id.type_out:
+				restaurant.setType("take_out");
+				break;
+				
+			case R.id.type_del:
+				restaurant.setType("delivery");
+				break;
+			}
 		}
 	};
 
