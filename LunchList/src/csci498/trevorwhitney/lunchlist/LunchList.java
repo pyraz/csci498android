eb1eaf6a88b5b10a4ccb744de372e7601ea24f81
Trevor Whitney <trevorjwhitney@gmail.com>
2012-09-04 16:56:13 -0600

    added custom fonts to EditViews

--- LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
+++ LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
@@ -2,6 +2,7 @@ package csci498.trevorwhitney.lunchlist;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,11 +10,22 @@ import android.widget.EditText;
 public class LunchList extends Activity {
 	
 	Restaurant restaurant = new Restaurant();
+	EditText name, address;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
+        
+        Typeface quicksand = Typeface.createFromAsset(getAssets(), 
+        		"fonts/quicksand.otf");
+        Typeface florante = Typeface.createFromAsset(getAssets(),
+        		"fonts/florante.ttf");
+        
+        name = (EditText)findViewById(R.id.name);
+        address = (EditText)findViewById(R.id.address);
+        name.setTypeface(quicksand);
+        address.setTypeface(florante);
         
         Button save = (Button)findViewById(R.id.save_btn);
         save.setOnClickListener(onSave);
@@ -21,9 +33,6 @@ public class LunchList extends Activity {
 
     private View.OnClickListener onSave = new View.OnClickListener() {	
 		public void onClick(View v) {
-			EditText name = (EditText)findViewById(R.id.name);
-			EditText address = (EditText)findViewById(R.id.address);
-			
 			restaurant.setName(name.getText().toString());
 			restaurant.setAddress(address.getText().toString());
 		}
