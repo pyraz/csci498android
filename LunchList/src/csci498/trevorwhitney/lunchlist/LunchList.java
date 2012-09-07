f460529e04495890654b3965a76047a8748fc7da
Trevor Whitney <trevorjwhitney@gmail.com>
2012-09-07 12:20:38 -0600

    added tabs with icons to LunchList

--- LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
+++ LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
@@ -3,7 +3,7 @@ package csci498.trevorwhitney.lunchlist;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +14,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TabHost;
 import android.widget.TextView;
 
-public class LunchList extends Activity {
+public class LunchList extends TabActivity {
 	
 	List<Restaurant> restaurants = new ArrayList<Restaurant>();
 	RestaurantAdapter adapter = null;
@@ -32,6 +33,20 @@ public class LunchList extends Activity {
 	  ListView list = (ListView)findViewById(R.id.restaurant_list);
 	  adapter = new RestaurantAdapter();
 	  list.setAdapter(adapter);
+	  
+	  TabHost.TabSpec spec = getTabHost().newTabSpec("tag1");
+	  spec.setContent(R.id.restaurant_list);
+	  spec.setIndicator("List", getResources().getDrawable(
+	  		R.drawable.list));
+	  getTabHost().addTab(spec);
+	  
+	  spec = getTabHost().newTabSpec("tag2");
+	  spec.setContent(R.id.details_form);
+	  spec.setIndicator("Details", getResources().getDrawable(
+	  		R.drawable.restaurant));
+	  getTabHost().addTab(spec);
+	  
+	  getTabHost().setCurrentTab(0);
 	}
 
 	private View.OnClickListener onSave = new View.OnClickListener() {	
