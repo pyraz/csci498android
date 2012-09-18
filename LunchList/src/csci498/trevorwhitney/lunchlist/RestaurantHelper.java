586e84c3cfc5a6c031a24de7eb3e7188ca758c6a
Trevor Whitney <trevorjwhitney@gmail.com>
2012-09-18 17:32:11 -0600

    Populated the onCreate method in RestaurantHelper to create DB

--- LunchList/src/csci498/trevorwhitney/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/trevorwhitney/lunchlist/RestaurantHelper.java
@@ -1,6 +1,7 @@
 package csci498.trevorwhitney.lunchlist;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
@@ -16,10 +17,14 @@ class RestaurantHelper extends SQLiteOpenHelper {
 	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE restaurants" +
+				"(_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT," +
+				"address TEXT, type TEXT, notes TEXT);");
 	}
 	
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion,
 			int newVersion) {
+		/* won't be needed until more than one schema versions exist */
 	}
 }
