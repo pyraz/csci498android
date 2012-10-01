a21310d11a53b627531bed4ac9265367b0a4ccb3
Trevor Whitney <trevorjwhitney@gmail.com>
2012-10-01 15:52:50 -0600

    Finished APT 15, implementing the RSS feed

--- LunchList/src/csci498/trevorwhitney/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/trevorwhitney/lunchlist/FeedActivity.java
@@ -1,18 +1,62 @@
 package csci498.trevorwhitney.lunchlist;
 
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
+import org.mcsoxford.rss.RSSReader;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 
 public class FeedActivity extends ListActivity {
+	
+	private InstanceState state = null;
+	public static final String FEED_URL = 
+			"csci498.trevorwhitney.lunchlist.FEED_URL";
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		state = (InstanceState)getLastNonConfigurationInstance();
+		
+		if (state == null) {
+			state = new InstanceState();
+			state.task = new FeedTask(this);
+			state.task.execute(getIntent().getStringExtra(FEED_URL));
+		}
+		else {
+			if (state.task != null) {
+				state.task.attach(this);
+			}
+			
+			if (state.feed != null) {
+				setFeed(state.feed);
+			}
+		}
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		if (state.task != null) {
+			state.task.detach();
+		}
+		
+		return state;
+	}
+	
+	private void setFeed(RSSFeed feed) {
+		state.feed = feed;
+		setListAdapter(new FeedAdapter(feed));
+	}
 	
 	private void goBlooey(Throwable t) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -25,7 +69,8 @@ public class FeedActivity extends ListActivity {
 	}
 	
 	private static class FeedTask extends 
-			AsyncTask<String, Void, Void> {
+			AsyncTask<String, Void, RSSFeed> {
+		private RSSReader reader = new RSSReader();
 		private Exception e = null;
 		private FeedActivity activity = null;
 		
@@ -33,33 +78,81 @@ public class FeedActivity extends ListActivity {
 			attach(activity);
 		}
 		
+		void attach(FeedActivity activity) {
+			this.activity = activity;
+		}
+		
+		void detach() {
+			this.activity = null;
+		}
+		
 		@Override
-		public Void doInBackground(String... urls) {
+		public RSSFeed doInBackground(String... urls) {
+			RSSFeed result = null;
+			
 			try {
-				DefaultHttpClient client = new DefaultHttpClient();
-				HttpGet getMethod = new HttpGet(urls[0]);
-				ResponseHandler<String> responseHandler = 
-						new BasicResponseHandler();
-				String responseBody = client.execute(getMethod, responseHandler);
-				
-				Log.d("FeedActivity", responseBody);
+				result = reader.load(urls[0]);
 			}
 			catch (Exception e) {
 				this.e = e;
 			}
 			
-			return null;
+			return result;
 		}
 		
 		@Override
-		public void onPostExectue(Void unused) {
+		public void onPostExecute(RSSFeed feed) {
 			if (e == null) {
-				// TODO
+				activity.setFeed(feed);
 			}
 			else {
 				Log.e("LunchList", "Exception parsing feed", e);
 				activity.goBlooey(e);
 			}
 		}
+	}
+	
+	private class FeedAdapter extends BaseAdapter {
+		
+		RSSFeed feed = null;
+		
+		FeedAdapter(RSSFeed feed) {
+			super();
+			
+			this.feed = feed;
+		}
+		
+		public int getCount() {
+			return feed.getItems().size();
+		}
+		
+		public Object getItem(int position) {
+			return feed.getItems().get(position);
+		}
+		
+		public long getItemId(int position) {
+			return position;
+		}
+		
+		public View getView(int position, View convertView, 
+				ViewGroup parent) {
+			View row = convertView;
+			
+			if (row==null) {
+				LayoutInflater inflater = getLayoutInflater();
+				
+				row = inflater.inflate(android.R.layout.simple_list_item_1, 
+						parent, false);
+			}
+			
+			RSSItem item = (RSSItem)getItem(position);
+			((TextView)row).setText(item.getTitle());
+			return row;
+		}
+	}
+	
+	private static class InstanceState {
+		RSSFeed feed = null;
+		FeedTask task =null;
 	}
 }
