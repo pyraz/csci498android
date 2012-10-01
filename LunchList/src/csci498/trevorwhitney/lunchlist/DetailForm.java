f16067d2dd8b80d0db090ac45590dea6c15438ea
Trevor Whitney <trevorjwhitney@gmail.com>
2012-10-01 12:32:29 -0600

    Added options menu for getting RSS feed

--- LunchList/src/csci498/trevorwhitney/lunchlist/DetailForm.java
+++ LunchList/src/csci498/trevorwhitney/lunchlist/DetailForm.java
@@ -1,12 +1,19 @@
 package csci498.trevorwhitney.lunchlist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class DetailForm extends Activity {
 	EditText name = null;
@@ -63,6 +70,41 @@ public class DetailForm extends Activity {
 		address.setText(savedInstanceState.getString("address"));
 		notes.setText(savedInstanceState.getString("notes"));
 		types.check(savedInstanceState.getInt("type"));
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.details_option, menu);
+		
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.feed) {
+			if (isNetworkAvailable()) {
+				Intent i = new Intent(this, FeedActivity.class);
+				
+				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+				startActivity(i);
+			}
+			else {
+				Toast.makeText(this, "Sorry, the Internet is not available",
+						Toast.LENGTH_LONG).show();
+			}
+			
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private boolean isNetworkAvailable() {
+		ConnectivityManager cm = 
+				(ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info = cm.getActiveNetworkInfo();
+		
+		return info != null;
 	}
 	
 	public void loadRestaurant() {
