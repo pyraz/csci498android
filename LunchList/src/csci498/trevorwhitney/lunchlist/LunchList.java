04a82f067c2efb1201bd1772e348c52a78096d32
Trevor Whitney <trevorjwhitney@gmail.com>
2012-10-01 10:45:54 -0600

    Finished basic preferences functionality

--- LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
+++ LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
@@ -3,8 +3,10 @@ package csci498.trevorwhitney.lunchlist;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,14 +25,16 @@ public class LunchList extends ListActivity {
 	RestaurantAdapter adapter = null;
 	Restaurant current = null;
 	RestaurantHelper helper = null;
+	SharedPreferences prefs = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
 	  setContentView(R.layout.activity_lunch_list);
 	  helper = new RestaurantHelper(this);
+	  prefs = PreferenceManager.getDefaultSharedPreferences(this);
 	  
-	  restaurants = helper.getAll();
+	  restaurants = helper.getAll(prefs.getString("sort_order", "name"));
 	  startManagingCursor(restaurants);
 	  adapter = new RestaurantAdapter(restaurants);
 	  setListAdapter(adapter);
@@ -68,6 +72,11 @@ public class LunchList extends ListActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (item.getItemId() == R.id.add) {
 			startActivity(new Intent(LunchList.this, DetailForm.class));
+			
+			return true;
+		}
+		else if (item.getItemId() == R.id.prefs) {
+			startActivity(new Intent(this, EditPreferences.class));
 			
 			return true;
 		}
