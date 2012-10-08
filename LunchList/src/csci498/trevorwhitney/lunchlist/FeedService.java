3fdee07c5dd6846945968be21b4f5b8d63fe2920
Trevor Whitney <trevorjwhitney@gmail.com>
2012-10-08 11:42:31 -0600

    Created FeedService class

---
+++ LunchList/src/csci498/trevorwhitney/lunchlist/FeedService.java
@@ -0,0 +1,15 @@
+package csci498.trevorwhitney.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+	public FeedService() {
+		super("FeedService");
+	}
+	
+	@Override
+	public void onHandleIntent(Intent i) {
+		//do something
+	}
+}
