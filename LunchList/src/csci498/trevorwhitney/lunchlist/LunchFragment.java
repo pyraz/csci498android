36cfcc261275f770c1978c40a1b8c2566b044f9c
Trevor Whitney <trevorjwhitney@gmail.com>
2012-11-14 18:44:02 -0700

    Moved LunchList into LunchFragment, refactored accordingly

---
+++ LunchList/src/csci498/trevorwhitney/lunchlist/LunchFragment.java
@@ -0,0 +1,138 @@
+package csci498.trevorwhitney.lunchlist;
+
+import android.support.v4.app.ListFragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+
+public class LunchFragment extends ListFragment {
+	
+	public final static String ID_EXTRA = 
+			"csci498.trevorwhitney.lunchlist._ID";
+	Cursor restaurants = null;
+	RestaurantAdapter adapter = null;
+	RestaurantHelper helper = null;
+	SharedPreferences prefs = null;
+	OnRestaurantListener listener = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  
+	  setHasOptionsMenu(true);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		helper = new RestaurantHelper(getActivity());
+	  prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+	  initList();
+	  prefs.registerOnSharedPreferenceChangeListener(prefListener);
+	}
+	
+	@Override
+	public void onPause() {
+		helper.close();
+		
+		super.onPause();
+	}
+	
+	@Override
+	public void onListItemClick(ListView list, View view, int position,
+			long id) {
+		if (listener != null) {
+			listener.onRestaurantSelected(id);
+		}
+	}
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, 
+			MenuInflater inflater) {
+		inflater.inflate(R.menu.option, menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.add) {
+			startActivity(new Intent(getActivity(), DetailForm.class));
+			
+			return true;
+		}
+		else if (item.getItemId() == R.id.prefs) {
+			startActivity(new Intent(getActivity(), EditPreferences.class));
+			
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void setOnRestaurantListener(OnRestaurantListener listener) {
+		this.listener = listener;
+	}
+	
+	private void initList() {
+		if (restaurants != null) {
+			restaurants.close();
+		}
+		
+		restaurants = helper.getAll(prefs.getString("sort_order", "name"));
+		adapter = new RestaurantAdapter(restaurants);
+		setListAdapter(adapter);
+	}
+	
+	private SharedPreferences.OnSharedPreferenceChangeListener prefListener = 
+			new SharedPreferences.OnSharedPreferenceChangeListener() {
+		public void onSharedPreferenceChanged(SharedPreferences sharedPrefs,
+				String key) {
+			if (key.equals("sort_order")) {
+				initList();
+			}
+		}
+	};
+	
+	public interface OnRestaurantListener {
+		void onRestaurantSelected(long id);
+	}
+	
+	class RestaurantAdapter extends CursorAdapter {
+		
+		@SuppressWarnings("deprecation")
+		RestaurantAdapter(Cursor c) {
+			super(getActivity(), c);
+		}
+		
+		@Override
+		public void bindView(View row, Context ctxt, Cursor c) {
+			RestaurantHolder holder = (RestaurantHolder)row.getTag();
+			holder.populateList(c, helper);
+		}
+		
+		@Override
+		public View newView(Context ctxt, Cursor c, ViewGroup parent) {
+			LayoutInflater inflater = getActivity().getLayoutInflater();
+			View row = inflater.inflate(R.layout.restuarant_row,
+					parent, false);
+			RestaurantHolder holder = new RestaurantHolder(row);
+			
+			row.setTag(holder);
+			
+			return row;
+		}
+		
+	}
+
+}
--- LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
+++ LunchList/src/csci498/trevorwhitney/lunchlist/LunchList.java
@@ -1,133 +1,28 @@
 package csci498.trevorwhitney.lunchlist;
 
-import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CursorAdapter;
-import android.widget.ListView;
+import android.support.v4.app.FragmentActivity;
 
-public class LunchList extends ListActivity {
-	
+public class LunchList extends FragmentActivity
+		implements LunchFragment.OnRestaurantListener {
 	public final static String ID_EXTRA = 
 			"csci498.trevorwhitney.lunchlist._ID";
 	
-	Cursor restaurants = null;
-	RestaurantAdapter adapter = null;
-	Restaurant current = null;
-	RestaurantHelper helper = null;
-	SharedPreferences prefs = null;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	  super.onCreate(savedInstanceState);
-	  setContentView(R.layout.activity_lunch_list);
-	  helper = new RestaurantHelper(this);
-	  prefs = PreferenceManager.getDefaultSharedPreferences(this);
-	  restaurants = helper.getAll(prefs.getString("sort_order", "name"));
-	  initList();
-	  prefs.registerOnSharedPreferenceChangeListener(prefListener);
-	}
-	
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lunch_list);
 		
-		helper.close();
+		LunchFragment lunch = (LunchFragment)getSupportFragmentManager()
+				.findFragmentById(R.id.lunch);
+		lunch.setOnRestaurantListener(this);
 	}
 	
-	@Override
-	public void onSaveInstanceState(Bundle state) {
-		super.onSaveInstanceState(state);
-	}
-	
-	@Override
-	public void onListItemClick(ListView list, View view, int position,
-			long id) {
-		Intent i = new Intent(LunchList.this, DetailForm.class);
+	public void onRestaurantSelected(long id) {
+		Intent i = new Intent(this, DetailForm.class);
 		
 		i.putExtra(ID_EXTRA, String.valueOf(id));
 		startActivity(i);
 	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		new MenuInflater(this).inflate(R.menu.option, menu);
-		
-		return super.onCreateOptionsMenu(menu);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == R.id.add) {
-			startActivity(new Intent(LunchList.this, DetailForm.class));
-			
-			return true;
-		}
-		else if (item.getItemId() == R.id.prefs) {
-			startActivity(new Intent(this, EditPreferences.class));
-			
-			return true;
-		}
-		
-		return super.onOptionsItemSelected(item);
-	}
-	
-	private void initList() {
-		if (restaurants != null) {
-			stopManagingCursor(restaurants);
-			restaurants.close();
-		}
-		
-		restaurants = helper.getAll(prefs.getString("sort_order", "name"));
-		startManagingCursor(restaurants);
-		adapter = new RestaurantAdapter(restaurants);
-		setListAdapter(adapter);
-	}
-	
-	private SharedPreferences.OnSharedPreferenceChangeListener prefListener = 
-			new SharedPreferences.OnSharedPreferenceChangeListener() {
-		public void onSharedPreferenceChanged(SharedPreferences sharedPrefs,
-				String key) {
-			if (key.equals("sort_order")) {
-				initList();
-			}
-		}
-	};
-	
-	class RestaurantAdapter extends CursorAdapter {
-		
-		RestaurantAdapter(Cursor c) {
-			super(LunchList.this, c);
-		}
-		
-		@Override
-		public void bindView(View row, Context ctxt, Cursor c) {
-			RestaurantHolder holder = (RestaurantHolder)row.getTag();
-			holder.populateList(c, helper);
-		}
-		
-		@Override
-		public View newView(Context ctxt, Cursor c, ViewGroup parent) {
-			LayoutInflater inflater = getLayoutInflater();
-			View row = inflater.inflate(R.layout.restuarant_row,
-					parent, false);
-			RestaurantHolder holder = new RestaurantHolder(row);
-			
-			row.setTag(holder);
-			
-			return row;
-		}
-		
-	}
-
 }
